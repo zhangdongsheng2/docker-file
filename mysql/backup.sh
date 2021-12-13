90d20e03a1da5552917fc3ef28c98efdc4359170
LAPTOP-P90NJHDM\zhangdongsheng <zhangdongsheng2@sina.com>
2021-12-13 10:45:16 +0800

    mysql 定时备份

--- mysql/backup.sh
+++ mysql/backup.sh
@@ -11,7 +11,7 @@ mysql_docker="mysql"
 mysql_charset="utf8" #MySQL编码
 backup_db_arr=("giot" "nacos-chongqing") #要备份的数据库名称，多个用空格分开隔开 如("db1" "db2" "db3")
 
-backup_location=/var/lib/mysql/backup #备份数据存放位置，末尾请不要带"/",此项可以保持默认，程序会自动创建文件夹
+backup_location=/var/lib/mysql/.backup #备份数据存放位置，末尾请不要带"/",此项可以保持默认，程序会自动创建文件夹
 expire_backup_delete="ON" #是否开启过期备份删除 ON为开启 OFF为关闭
 expire_days=7 #过期时间天数 默认为三天，此项只有在expire_backup_delete开启时有效
 
